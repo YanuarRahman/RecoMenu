50300574d7ae3c2475c8337e04c053c310e51016
YanuarRahman <rmyanuar04@gmail.com>
2023-05-31 13:51:28 +0700

    origin master

--- backend/controllers/Menus.js
+++ backend/controllers/Menus.js
@@ -1,15 +1,50 @@
+import { response } from "express";
 import Menu from "../models/MenuModel.js";
+import User from "../models/UserModel.js";
 
-export const getMenus = (req , res) =>{
-
+export const getMenus = async(req , res) =>{
+    try{
+        let response;
+        if(req.role === "admin"){
+            response = await Menu.findAll({
+                include:[{
+                    model: User
+                }]
+            });
+        }else{
+            response = await Menu.findAll({
+                where:{
+                    userId: req.userId
+                },
+                include:[{
+                    model: User
+                }]
+            });
+        }
+        res.status(200).json(response);
+    }catch (error){
+        res.status(500).json({msg: error.message});
+    }
 }
 
 export const getMenuById = (req , res) =>{
     
 }
 
-export const createMenu = (req , res) =>{
-    
+export const createMenu = async(req, res) =>{
+    const {nama, rasa, qty, harga} = req.body;
+    try {
+        await Menu.create({
+            nama: nama,
+            rasa: rasa,
+            qty: qty,
+            harga: harga,
+            userId: req.userId
+        });
+        res.status(201).json({msg: "Menu Berhasil Dibuat"});
+    } catch (error) {
+        res.status(500).json({msg: error.message});
+    }
 }
 
 export const updateMenu = (req , res) =>{
--- backend/routes/MenuRoute.js
+++ backend/routes/MenuRoute.js
@@ -7,15 +7,15 @@ import {
     updateMenu,
     deleteMenu,
     } from "../controllers/Menus.js"
-
+import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 // endpoint
-router.get('/menus',getMenus);
-router.get('/menus/:id',getMenuById);
-router.post('/menus/',createMenu);
-router.patch('/menus/:id',updateMenu);
-router.delete('/menus/:id',deleteMenu);
+router.get('/menus',verifyUser,getMenus);
+router.get('/menus/:id',verifyUser,getMenuById);
+router.post('/menus/',verifyUser,createMenu);
+router.patch('/menus/:id',verifyUser,updateMenu);
+router.delete('/menus/:id',verifyUser,deleteMenu);
 
 
 
